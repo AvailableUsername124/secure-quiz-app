caca525b57b91994c89a323edfa5ec7475953b03
AvailableUsername124 <kaylebhanacek@gmail.com>
2025-11-13 14:20:31 -0800

    Feat: stylized it a bit

--- webapp.py
+++ webapp.py
@@ -2,14 +2,16 @@ import os
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
+import secrets
 
 app = Flask(__name__)
 
 # In order to use "sessions",you need a "secret key".
 # This is something random you generate.  
 # For more info see: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
-
-app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
+key = secrets.token_urlsafe(16)
+app.secret_key = os.environ["SECRET_KEY"]#This is an environment variable.  
+#run set SECRET_KEY=your_random_secret_key to get cmd prompt to run it.
                                      #The value should be set on the server. 
                                      #To run locally, set in env.bat (env.sh on Macs) and include that file in gitignore so the secret key is not made public.
 
@@ -38,4 +40,4 @@ def renderPage3():
     return render_template('page3.html')
     
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
